c85f0bdf0680645db9236bf4cd9d0085588de18a
Tanushka PC <tanu31195@users.noreply.github.com>
2019-05-13 02:26:22 +0530

    Changes

--- app/src/main/java/io/github/tanu31195/audionirvana/EarphoneQuestionsActivity.java
+++ app/src/main/java/io/github/tanu31195/audionirvana/EarphoneQuestionsActivity.java
@@ -6,8 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class EarphoneQuestionsActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroupQ1;
+    private RadioGroup radioGroupQ2;
+    private RadioButton radioAnsButton;
+    private Button btnAnalyze;
+    private List<RadioGroup> earphoneQuestions = new ArrayList<>();
+    Map<String, String> answerMap = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +39,53 @@ public class EarphoneQuestionsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+//        addListenerOnButton();
     }
+
+//    public void addListenerOnButton() {
+//
+//        btnAnalyze = (Button) findViewById(R.id.btnAnalyze);
+//
+////        earphoneQuestions.add((RadioGroup) findViewById(R.id.question1));
+////        earphoneQuestions.add((RadioGroup) findViewById(R.id.question2));
+//
+////        int id = getResources().getIdentifier("button_"+i, "id", getPackageName());
+////        button[i] = (RadioGroup) findViewById(id);
+////
+////        earphoneQuestions.add(AudioNirvanaUtils.getAll(getResources(), getPackageName());
+//
+//
+//
+//
+//        btnAnalyze.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                radioGroupQ1 = (RadioGroup) findViewById(R.id.question1);
+//                radioGroupQ2 = (RadioGroup) findViewById(R.id.question2);
+//                earphoneQuestions.add(radioGroupQ1);
+//                earphoneQuestions.add(radioGroupQ2);
+//                int questionNumber = 1;
+//                for ( RadioGroup earphoneQuestion : earphoneQuestions ) {
+//
+//
+//                    // get selected radio button from radioGroup
+//                    int selectedId = earphoneQuestion.getCheckedRadioButtonId();
+//                    // find the radiobutton by returned id
+//                    radioAnsButton = (RadioButton) findViewById(selectedId);
+//
+//                    answerMap.put( String.valueOf(questionNumber) , String.valueOf(radioAnsButton.getText()));
+//                    questionNumber++;
+//                }
+////                    // get selected radio button from radioGroup
+////                    int selectedId = radioGroupQ1.getCheckedRadioButtonId();
+////                    // find the radiobutton by returned id
+////                    radioAnsButton = (RadioButton) findViewById(selectedId);
+////                Toast.makeText(MainActivity.this,
+////                        radioAnsButton.getText(), Toast.LENGTH_SHORT).show();
+//            }
+//
+//        });
+//    }
 }
--- app/src/main/java/io/github/tanu31195/audionirvana/MainActivity.java
+++ app/src/main/java/io/github/tanu31195/audionirvana/MainActivity.java
@@ -15,14 +15,6 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RadioGroup radioGroupQ1;
-    private RadioGroup radioGroupQ2;
-    private RadioButton radioAnsButton;
-    private Button btnAnalyze;
-    private List<RadioGroup> earphoneQuestions = new ArrayList<>();
-    Map<String, String> answerMap = new HashMap<String, String>();
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,53 +56,6 @@ public class MainActivity extends AppCompatActivity {
                 Intent mainIntent = new Intent(MainActivity.this,AboutUs.class);
                 startActivity(mainIntent);
             }
-        });
-
-        addListenerOnButton();
-    }
-
-    public void addListenerOnButton() {
-
-        btnAnalyze = (Button) findViewById(R.id.btnAnalyze);
-//        earphoneQuestions.add((RadioGroup) findViewById(R.id.question1));
-//        earphoneQuestions.add((RadioGroup) findViewById(R.id.question2));
-
-//        int id = getResources().getIdentifier("button_"+i, "id", getPackageName());
-//        button[i] = (RadioGroup) findViewById(id);
-//
-//        earphoneQuestions.add(AudioNirvanaUtils.getAll(getResources(), getPackageName());
-
-
-
-
-        btnAnalyze.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                radioGroupQ1 = (RadioGroup) findViewById(R.id.question1);
-                radioGroupQ2 = (RadioGroup) findViewById(R.id.question2);
-                earphoneQuestions.add(radioGroupQ1);
-                earphoneQuestions.add(radioGroupQ2);
-                int questionNumber = 1;
-                for ( RadioGroup earphoneQuestion : earphoneQuestions ) {
-
-
-                    // get selected radio button from radioGroup
-                    int selectedId = earphoneQuestion.getCheckedRadioButtonId();
-                    // find the radiobutton by returned id
-                    radioAnsButton = (RadioButton) findViewById(selectedId);
-
-                    answerMap.put( String.valueOf(questionNumber) , String.valueOf(radioAnsButton.getText()));
-                    questionNumber++;
-                }
-//                    // get selected radio button from radioGroup
-//                    int selectedId = radioGroupQ1.getCheckedRadioButtonId();
-//                    // find the radiobutton by returned id
-//                    radioAnsButton = (RadioButton) findViewById(selectedId);
-//                Toast.makeText(MainActivity.this,
-//                        radioAnsButton.getText(), Toast.LENGTH_SHORT).show();
-            }
-
         });
     }
 }
